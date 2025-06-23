3899bebe5ef99a7f9e1a3583bc407cb3daa9d984
Saumya Gupta <gupta.sg003@gmail.com>
2025-06-23 19:56:22 +0530

    Update complaintRoutes.js

--- api/routes/complaintRoutes.js
+++ api/routes/complaintRoutes.js
@@ -3,13 +3,9 @@ const router = express.Router();
 const complaintController = require('../controllers/complaintController');
 
 router.post('/', complaintController.submitComplaint);
-
 router.get('/', complaintController.getAllComplaints);
-
 router.post('/assign', complaintController.assignComplaint);
-
 router.post('/update', complaintController.updateComplaint);
-
 router.get('/user/:userId', complaintController.getComplaintsByUser);
 
 module.exports = router;
